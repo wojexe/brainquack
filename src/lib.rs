37b833a221c184f9c146eb65e273d4f4c09244e9
Wojciech Sierotowicz (wojexe) <21208490+wojexe@users.noreply.github.com>
2023-04-14 20:42:50 +0200

    Reinforced error handling add main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,49 @@
 pub mod interpreter;
 pub mod lexer;
 
-pub use interpreter as BF_Interpreter;
-pub use lexer as BF_Lexer;
+pub use interpreter::*;
+pub use lexer::*;
+
+#[derive(Debug)]
+pub enum BrainQuackError {
+    LexerError(LexerError),
+    InterpreterError(InterpreterError),
+}
+
+impl std::error::Error for BrainQuackError {}
+
+impl std::fmt::Display for BrainQuackError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            BrainQuackError::LexerError(e) => write!(f, "{e}"),
+            BrainQuackError::InterpreterError(e) => write!(f, "{e}"),
+        }
+    }
+}
+
+impl From<LexerError> for BrainQuackError {
+    fn from(value: LexerError) -> Self {
+        BrainQuackError::LexerError(value)
+    }
+}
+
+impl From<InterpreterError> for BrainQuackError {
+    fn from(value: InterpreterError) -> Self {
+        BrainQuackError::InterpreterError(value)
+    }
+}
+
+pub fn execute<S>(code: S, output: Option<&mut String>) -> Result<(), BrainQuackError>
+where
+    S: AsRef<str>,
+{
+    let mut instructions = lexer::parse(code)?;
+
+    interpreter::interpret(
+        &mut instructions,
+        &mut vec![std::num::Wrapping(0u8); 30_000],
+        output,
+    )?;
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use brainquack::*;
+
+fn main() -> Result<(), BrainQuackError> {
+    let hello_world = lexer::parse(include_str!("programs/helloWorld.bf"))?;
+    let cell_size = lexer::parse(include_str!("programs/cellSize.bf"))?;
+
+    let mut memory = vec![std::num::Wrapping(0); 30_000];
+
+    if let Err(e) = interpreter::interpret(&hello_world, &mut memory, None) {
+        println!("{e}");
+    }
+
+    memory.fill(std::num::Wrapping(0));
+
+    if let Err(e) = interpreter::interpret(&cell_size, &mut memory, None) {
+        println!("{e}");
+    }
+
+    Ok(())
+}
